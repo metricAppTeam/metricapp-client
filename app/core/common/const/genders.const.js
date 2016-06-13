c415d6f48f203a5905e618164b0de1aa2b8ac6f0
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-06-13 23:32:24 +0200

    bugfix for string equality, genders constants for signup

---
+++ app/core/common/const/genders.const.js
@@ -0,0 +1,20 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc overview
+* @name ROLES
+* @module metricapp
+* @description
+* Defines constants related to user genders.
+* - MALE:
+* - FEMALE:
+************************************************************************************/
+
+angular.module('metricapp')
+
+.constant('GENDERS', {
+    MALE:   'MALE',
+    FEMALE: 'FEMALE'
+});
+
+})();
