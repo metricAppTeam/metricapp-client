5a6715c3ca7561ea13e0ddbe41b59f3eaa05fb3f
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-06-17 00:46:35 +0200

    app events mockups

---
+++ app/core/common/events/goal.events.const.js
@@ -0,0 +1,22 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc constant
+* @name TEAM_EVENTS
+* @module metricapp
+* @description
+* Defines events related to team management.
+* - TEAM_EVENT_A:
+* - TEAM_EVENT_B:
+* - TEAM_EVENT_C:
+************************************************************************************/
+
+angular.module('metricapp')
+
+.constant('TEAM_EVENTS', {
+    TEAM_EVENT_A: 'team.event.a',
+    TEAM_EVENT_B: 'team.event.b',
+    TEAM_EVENT_C: 'team.event.c'
+});
+
+})();
---
+++ app/core/common/events/question.events.const.js
@@ -0,0 +1,22 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc constant
+* @name QUESTION_EVENTS
+* @module metricapp
+* @description
+* Defines events related to questions.
+* - QUESTION_EVENT_A:
+* - QUESTION_EVENT_B:
+* - QUESTION_EVENT_C:
+************************************************************************************/
+
+angular.module('metricapp')
+
+.constant('QUESTION_EVENTS', {
+    QUESTION_EVENT_A: 'question.event.a',
+    QUESTION_EVENT_B: 'question.event.b',
+    QUESTION_EVENT_C: 'question.event.c'
+});
+
+})();
