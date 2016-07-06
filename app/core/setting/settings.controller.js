e6644b1f4fdd223cf029e3639039ff28c185ae69
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-06 02:58:46 +0200

    setting dynamic view mockup

--- app/core/setting/settings.controller.js
+++ app/core/setting/settings.controller.js
@@ -6,21 +6,42 @@
 * @module metricapp
 * @requires $scope
 * @requires $location
+* @requires SettingService
 *
 * @description
-* Manages the whole app settings.
-* Realizes the control layer for {settings.view}.
+* Realizes the control layer for `settings.view`.
 ************************************************************************************/
 
 angular.module('metricapp')
 
 .controller('SettingsController', SettingsController);
 
-SettingsController.$inject = ['$scope', '$location', 'AuthService'];
+SettingsController.$inject = ['$scope', '$location', 'SettingService'];
 
-function SettingsController($scope, $location, AuthService) {
+function SettingsController($scope, $location, SettingService) {
 
     var vm = this;
+
+    _init();
+
+    function _loadSettings() {
+        vm.loading = true;
+        vm.success = false;
+        SettingService.getSettings().then(
+            function(response) {
+                vm.loading = false;
+                vm.success = true;
+            }
+        );
+    }
+
+    function _init() {
+        vm.loading = true;
+        vm.success = false;
+        vm.settings = {};
+        vm.numsettings = 0;
+        _loadSettings();
+    }
 
 }
 
