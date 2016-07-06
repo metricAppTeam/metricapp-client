dc52995e85301fc6068a866271066517713cc443
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-06 02:59:28 +0200

    team dynamic view mockup

--- app/core/team/teams.controller.js
+++ app/core/team/teams.controller.js
@@ -2,33 +2,32 @@
 
 /************************************************************************************
 * @ngdoc controller
-* @name TeamController
+* @name TeamsController
 * @module metricapp
 * @requires $scope
 * @requires $location
-* @requires $routeParams
 * @requires TeamService
 *
 * @description
-* Realizes the control layer for `team.view`.
+* Realizes the control layer for `teams.view`.
 ************************************************************************************/
 
 angular.module('metricapp')
 
-.controller('TeamController', TeamController);
+.controller('TeamsController', TeamsController);
 
-TeamController.$inject = ['$scope', '$location', '$routeParams', 'TeamService'];
+TeamsController.$inject = ['$scope', '$location', 'TeamService'];
 
-function TeamController($scope, $location, $routeParams, TeamService) {
+function TeamsController($scope, $location, TeamService) {
 
     var vm = this;
 
     _init();
 
-    function _loadTeam(teamid) {
+    function _loadTeams(tmStart, tmN) {
         vm.loading = true;
         vm.success = false;
-        TeamService.getTeam(teamid).then(
+        TeamService.getTeams(tmStart, tmN).then(
             function(response) {
                 vm.loading = false;
                 vm.success = true;
@@ -39,13 +38,9 @@ function TeamController($scope, $location, $routeParams, TeamService) {
     function _init() {
         vm.loading = true;
         vm.success = false;
-        if (!$routeParams.teamid) {
-            $location.path('/teams');
-        }
-        vm.currTeam = {
-            id: $routeParams.teamid
-        };
-        _loadTeam(vm.currTeam.id);
+        vm.teams = [];
+        vm.numteams = 0;
+        _loadTeams(0, 20);
     }
 
 }
