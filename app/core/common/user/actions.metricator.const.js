0e1b6e79a1a69ab26f0a5253f0653b398f415d85
micheleporretta <micheleporretta@mbpdimichele2>
2016-07-06 09:56:12 +0200

    Metricator Sidebar bug fixed

--- app/core/common/user/actions.metricator.const.js
+++ app/core/common/user/actions.metricator.const.js
@@ -14,7 +14,7 @@
 angular.module('metricapp')
 
 .constant('METRICATOR_ACTIONS', [
-    {name: 'Profile', href: '#/profile'},
+    {name: 'Profile',  			  href: '#/profile'},
     {name: 'METRICATOR_ACTION_2', href: '#/metricator_action_2'},
     {name: 'Settings', href: '#/settings'}
 ]);
