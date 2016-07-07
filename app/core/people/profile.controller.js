861153d4dc26dc9c8f595eb5edb8d70cee4f1d69
micheleporretta <micheleporretta@10.200.2.134>
2016-07-07 10:55:26 +0200

    View Statistics Chart draft

--- app/core/people/profile.controller.js
+++ app/core/people/profile.controller.js
@@ -29,6 +29,38 @@ function ProfileController($scope, $location, ProfileService, FlashService, GEND
 
     vm.GENDERS = GENDERS;
 
+    vm.config = {
+        title: 'Products',
+        tooltips: true,
+        labels: false,
+        mouseover: function() {},
+        mouseout: function() {},
+        click: function() {},
+        legend: {
+          display: true,
+          //could be 'left, right'
+          position: 'right'
+        }
+      };
+
+    vm.data = {
+        series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+        data: [{
+          x: "Laptops",
+          y: [100, 500, 0],
+          tooltip: "this is tooltip"
+        }, {
+          x: "Desktops",
+          y: [300, 100, 100]
+        }, {
+          x: "Mobiles",
+          y: [351]
+        }, {
+          x: "Tablets",
+          y: [54, 0, 879]
+        }]
+      };
+
     //load picture
     vm.picture = 'assets/images/users/user2.jpg';
 
