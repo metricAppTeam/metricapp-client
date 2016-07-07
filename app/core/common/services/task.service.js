5496c8eed79dab698e41cf6c9522c651c13c3dd8
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-08 00:58:57 +0200

    task management services ready for nested promises

--- app/core/common/services/task.service.js
+++ app/core/common/services/task.service.js
@@ -15,14 +15,14 @@ angular.module('metricapp')
 
 .service('TaskService', TaskService);
 
-TaskService.$inject = ['$http', 'REST_SERVICE'];
+TaskService.$inject = ['$http', '$q', 'REST_SERVICE', 'DB_TASKS'];
 
-function TaskService($http, REST_SERVICE) {
+function TaskService($http, $q, REST_SERVICE, DB_TASKS) {
 
     var service = this;
 
     service.getTask = getTask;
-    service.getTasks = getTasks;
+    service.getNTasksFrom = getNTasksFrom;
 
     /********************************************************************************
     * @ngdoc method
@@ -30,25 +30,44 @@ function TaskService($http, REST_SERVICE) {
     * @description
     * Retrieves the specified task.
     * @param {Int} taskid The task id.
-    * @returns {RichTask|Error} On success, the notifications with meta; an error,
-    * otherwise.
+    * @returns {Task|Error} On success, the task; an error message, otherwise.
     ********************************************************************************/
     function getTask(taskid) {
-
+        return $q(function(resolve, reject) {
+            setTimeout(function() {
+                DB_TASKS.forEach(function(TASK) {
+                    if (TASK.id === elemid) {
+                        resolve({task: TASK});
+                        return;
+                    }
+                });
+                reject({errmsg: 'Task ' + taskid + ' not found'});
+            }, 500);
+        });
     }
 
     /********************************************************************************
     * @ngdoc method
-    * @name getTasks
+    * @name getNTasksFrom
     * @description
     * Retrieves the specified tasks for authuser.
     * @param {Int} tskStart First task index.
     * @param {Int} tskN Number of tasks.
-    * @returns {[RichTask]|Error} On success, the list of tasks with meta for
-    * authuser; an error, otherwise.
+    * @returns {[Task]|Error} On success, the list of tasks; an error message,
+    * otherwise.
     ********************************************************************************/
-    function getTasks(tskStart, tskN) {
-
+    function getNTasksFrom(tskStart, tskN) {
+        return $q(function(resolve, reject) {
+            setTimeout(function() {
+                var tasks = [];
+                var numtasks = DB_TASKS.length;
+                var end = (tskN == -1) ? numtasks : Math.min(tskStart + tskN, numtasks);
+                for (var i = tskStart; i < end; i++ ) {
+                    tasks.push(DB_TASKS[i]);
+                }
+                resolve({tasks: tasks, numtasks: numtasks});
+            }, 500);
+        });
     }
 
 }
