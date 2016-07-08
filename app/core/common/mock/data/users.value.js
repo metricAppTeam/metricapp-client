b1ee98d32493380cde08b8cb0ed2ca9e2f3c2516
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-08 02:15:21 +0200

    users model and data

--- app/core/common/mock/data/users.value.js
+++ app/core/common/mock/data/users.value.js
@@ -11,46 +11,50 @@
 
 angular.module('metricapp')
 
-.value('DB_USERS', [
-    {
+.value('DB_USERS', {
+
+    'expert': {
         username: 'expert',
         password: 'password',
         role: 'EXPERT',
         firstname:  'Giacomo',
         lastname:   'Marciani',
         gender:     'MALE',
-        birthday:   '27/06/1990',
+        birthday:   new Date(1990,06, 27),
         email:      'giacomo.marciani@domain.com',
         mobile:     '+39 12345678',
-        picture:    'uploads/profile-picture.png',
+        picture:    'uploads/user-picture-1.svg',
         online: false
     },
-    {
-        username: 'questioner',
-        password: 'password',
-        role: 'QUESTIONER',
-        firstname: 'Marco',
-        lastname: 'Piu',
-        gender:     'MALE',
-        birthday:   '27/06/1990',
-        email:      'marco.piu@domain.com',
-        mobile:     '+39 12345678',
-        picture:    'uploads/profile-picture.png',
-        online: false
-    },
-    {
+
+    'metricator': {
         username: 'metricator',
         password: 'password',
         role: 'METRICATOR',
         firstname: 'Andrea',
         lastname: 'Gennusa',
         gender:     'MALE',
-        birthday:   '27/06/1990',
+        birthday:   new Date(1990,06, 27),
         email:      'andrea.gennusa@domain.com',
         mobile:     '+39 12345678',
-        picture:    'uploads/profile-picture.png',
+        picture:    'uploads/user-picture-2.svg',
+        online: false
+    },
+
+    'questioner': {
+        username: 'questioner',
+        password: 'password',
+        role: 'QUESTIONER',
+        firstname: 'Marco',
+        lastname: 'Piu',
+        gender:     'MALE',
+        birthday:   new Date(1990,06, 27),
+        email:      'marco.piu@domain.com',
+        mobile:     '+39 12345678',
+        picture:    'uploads/user-picture-3.svg',
         online: false
     }
+
 ]);
 
 })();
