584ff97339ff19e0279b55d8a53b6011c74a7fb1
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-09 18:19:41 +0200

    code refactor to not collide with Michele, and user analytics

---
+++ app/core/user/profile.controller.js
@@ -0,0 +1,56 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc controller
+* @name ProfileController
+* @module metricapp
+* @requires $location
+* @requires $routeParams
+* @requires UserService
+*
+* @description
+* Realizes the control layer for `profile.view`.
+************************************************************************************/
+
+angular.module('metricapp')
+
+.controller('ProfileController', ProfileController);
+
+ProfileController.$inject = ['$location', '$routeParams', 'UserService'];
+
+function ProfileController($location, $routeParams, UserService) {
+
+    var vm = this;
+
+    _init();
+
+    function _loadUser(username) {
+        vm.loading = true;
+        vm.success = false;
+        UserService.getById(username).then(
+            function(resolve) {
+                vm.currUser = angular.copy(resolve.user);
+                vm.success = true;
+            },
+            function(reject) {
+                vm.errmsg = reject.errmsg;
+                vm.success = false;
+            }
+        ).finally(function() {
+            vm.loading = false;
+        });
+    }
+
+    function _init() {
+        vm.loading = true;
+        vm.success = false;
+        vm.errmsg = null;
+        vm.currUser = {
+            username: $routeParams.username
+        };
+        _loadUser(vm.currUser.username);
+    }
+
+}
+
+})();
