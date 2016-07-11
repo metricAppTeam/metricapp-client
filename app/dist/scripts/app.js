f9757faea702662ca7265f15a4e00f2c911dd4fd
alessandro.fazio <agil3b3ast@hotmail.it>
2016-07-12 00:48:13 +0200

    Got working remove item in a measurement goal metric list
    Now ng-show works with vm.metrics.length (assume it is not undefined)

--- app/dist/scripts/app.js
+++ app/dist/scripts/app.js
@@ -1855,6 +1855,66 @@ function topbar() {
 
 /************************************************************************************
 * @ngdoc controller
+* @name CommentController
+* @module metricapp
+* @requires $scope
+* @requires $location
+*
+* @description
+* Manages the comment-based conversation between users.
+* Realizes the control layer for `comment.view`.
+************************************************************************************/
+
+angular.module('metricapp')
+
+.controller('CommentController', CommentController);
+
+CommentController.$inject = ['$scope', '$location'];
+
+function CommentController($scope, $location) {
+
+    var vm = this;
+
+    vm.foo = foo;
+
+    /********************************************************************************
+    * @ngdoc method
+    * @name foo
+    * @description
+    * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    * eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    * @param {type} a Insert here param description.
+    * @param {type} b Insert here param description.
+    * @param {type} c Insert here param description.
+    * @returns {type} Insert here return description.
+    ********************************************************************************/
+    function foo(a, b, c) {
+
+    }
+
+    /********************************************************************************
+    * @ngdoc method
+    * @name _foo
+    * @description
+    * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    * eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    * @param {type} a Insert here param description.
+    * @param {type} b Insert here param description.
+    * @param {type} c Insert here param description.
+    * @returns {type} Insert here return description.
+    ********************************************************************************/
+    function _foo(a, b, c) {
+
+    }
+
+}
+
+})();
+
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc controller
 * @name LoginController
 * @requires $rootScope
 * @requires $location
@@ -2063,66 +2123,6 @@ function SignupController($scope, $location, SignupService, FlashService, ROLES,
     ********************************************************************************/
     function _init() {
         vm.loading = false;
-    }
-
-}
-
-})();
-
-(function() { 'use strict';
-
-/************************************************************************************
-* @ngdoc controller
-* @name CommentController
-* @module metricapp
-* @requires $scope
-* @requires $location
-*
-* @description
-* Manages the comment-based conversation between users.
-* Realizes the control layer for `comment.view`.
-************************************************************************************/
-
-angular.module('metricapp')
-
-.controller('CommentController', CommentController);
-
-CommentController.$inject = ['$scope', '$location'];
-
-function CommentController($scope, $location) {
-
-    var vm = this;
-
-    vm.foo = foo;
-
-    /********************************************************************************
-    * @ngdoc method
-    * @name foo
-    * @description
-    * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    * eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    * @param {type} a Insert here param description.
-    * @param {type} b Insert here param description.
-    * @param {type} c Insert here param description.
-    * @returns {type} Insert here return description.
-    ********************************************************************************/
-    function foo(a, b, c) {
-
-    }
-
-    /********************************************************************************
-    * @ngdoc method
-    * @name _foo
-    * @description
-    * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    * eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    * @param {type} a Insert here param description.
-    * @param {type} b Insert here param description.
-    * @param {type} c Insert here param description.
-    * @returns {type} Insert here return description.
-    ********************************************************************************/
-    function _foo(a, b, c) {
-
     }
 
 }
