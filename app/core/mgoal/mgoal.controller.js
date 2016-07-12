70899bdc80540ed2c131b080a6dcd72da5e8cf47
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-12 03:09:59 +0200

    view mock for mgoals and mgoal

---
+++ app/core/mgoal/mgoal.controller.js
@@ -0,0 +1,60 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc controller
+* @name MGoalService
+* @module metricapp
+* @requires $scope
+* @requires $location
+* @requires $routeParams
+* @requires MGoalService
+*
+* @description
+* Realizes the control layer for `mgoal.view` and `update-mgoal.view`.
+************************************************************************************/
+
+angular.module('metricapp')
+
+.controller('MGoalService', MGoalService);
+
+MGoalService.$inject = ['$scope', '$location', '$routeParams', 'MGoalService'];
+
+function MGoalService($scope, $location, $routeParams, MGoalService) {
+
+    var vm = this;
+
+    _init();
+
+    function _loadMGoal(goalid) {
+        vm.loading = true;
+        vm.success = false;
+        MGoalService.getById(goalid).then(
+            function(resolve) {
+                vm.currMGoal = angular.copy(resolve.mgoal);
+                vm.updtMGoal = angular.copy(vm.currMGoal);
+                vm.success = true;
+            },
+            function(reject) {
+                vm.errmsg = reject.errmsg;
+                vm.success = false;
+            }
+        ).finally(function() {
+            vm.loading = false;
+        });
+    }
+
+    function _init() {
+        vm.loading = true;
+        vm.success = false;
+        vm.errmsg = null;
+        if (!$routeParams.goalid) {
+            $location.path('/mgoals');
+        }
+        vm.currMGoal = {
+            id: $routeParams.goalid
+        };
+        _loadMGoal(vm.currMGoal.id);
+    }
+}
+
+})();
