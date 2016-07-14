7208660f846d0d2b7b79630207ab44d9182e6a4f
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-14 20:35:29 +0200

    bugfix in user property

--- app/core/common/filters/user.filter.js
+++ app/core/common/filters/user.filter.js
@@ -21,7 +21,7 @@ function userFilterByName() {
         }
         query.toLowerCase();
         users.forEach(function(user) {
-            if (user.fname.toLowerCase().indexOf(query)>=0 || user.lname.toLowerCase().indexOf(query)>=0) {
+            if (user.firstname.toLowerCase().indexOf(query)>=0 || user.lastname.toLowerCase().indexOf(query)>=0) {
                 result.push(user);
             }
         });
