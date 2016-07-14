8f166fcb3567abf1116bcddd746a0f7797a1634d
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-14 21:14:57 +0200

    excerpt filter to extract summary text with fallback string

---
+++ app/core/common/filters/excerpt.filter.js
@@ -0,0 +1,29 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc filter
+* @name excerptFilter
+* @module metricapp
+*
+* @description
+* Realizes string excerpt.
+************************************************************************************/
+
+angular.module('metricapp')
+
+app.filter('excerptFilter',excerptFilter);
+
+function excerptFilter(){
+  return function(string, condition, lenTrue, lenFalse, fallbackString){
+    if(!string){
+      return fallbackString;
+    }
+    if (condition) {
+      return string.toString().substring(0, lenTrue) + '...';
+    } else {
+      return string.toString().substring(0, lenFalse) + '...';
+    }
+  };
+}
+
+})();
