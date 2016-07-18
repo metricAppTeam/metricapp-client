e6ea29033a1b1b139f458f4b39be313a947a6635
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-18 10:32:55 +0200

    removed interceptor for 404, it will be handled by routes

---
+++ app/core/common/config/interceptors/interceptors.config.js
@@ -0,0 +1,26 @@
+(function() { 'use strict';
+
+/************************************************************************************
+* @ngdoc overview
+* @name config.interceptors
+* @module metricapp
+* @requires $httpProvider
+*
+* @description
+* Defines the HTTP interceptors for the whole app.
+************************************************************************************/
+
+angular.module('metricapp')
+
+.config(interceptors);
+
+interceptors.$inject = ['$httpProvider'];
+
+function interceptors($httpProvider) {
+
+    $httpProvider.interceptors.push('Error401Interceptor');
+
+    $httpProvider.interceptors.push('Error500Interceptor');
+}
+
+})();
