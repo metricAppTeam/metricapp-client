0d598527990969662412ca076dd64b7fc7ae1882
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-07-19 02:03:52 +0200

    rest call for user analytics

--- app/core/common/services/user-analytics.service.js
+++ app/core/common/services/user-analytics.service.js
@@ -5,9 +5,7 @@
 * @name UserAnalyticsService
 * @module metricapp
 * @requires $http
-* @requires $cookies
 * @requires REST_SERVICE
-* @requires USER_ANALYTICS
 *
 * @description
 * Realizes the user analytics DAO.
@@ -17,9 +15,9 @@ angular.module('metricapp')
 
 .service('UserAnalyticsService', UserAnalyticsService);
 
-UserAnalyticsService.$inject = ['$http', '$q', '$cookies', 'REST_SERVICE', 'USER_ANALYTICS', 'DB_USERS'];
+UserAnalyticsService.$inject = ['$http', '$q', 'REST_SERVICE'];
 
-function UserAnalyticsService($http, $q, $cookies, REST_SERVICE, USER_ANALYTICS, DB_USERS) {
+function UserAnalyticsService($http, $q, REST_SERVICE) {
 
     var service = this;
 
@@ -35,48 +33,24 @@ function UserAnalyticsService($http, $q, $cookies, REST_SERVICE, USER_ANALYTICS,
     * an error message, otherwise.
     ********************************************************************************/
     function getAll(username) {
-        return $q(function(resolve, reject) {
-            setTimeout(function() {
-                var USER = DB_USERS[username];
-                if (USER) {
-                    var analytics = {};
-                    // start compute analytics
-                    var gqmScore = Math.floor((Math.random() * 100) + 0);
-                    var gridsTot = Math.floor((Math.random() * 100) + 0);
-                    var artifactsTot = Math.floor((Math.random() * 100) + 0);
-                    var acceptanceRatio = Math.floor((Math.random() * 100) + 0);
-                    var artifactsMGoal = Math.floor((Math.random() * 100) + 0);
-                    var artifactsRate = Math.floor((Math.random() * 100) + 0);
-                    var worktimeRatio = Math.floor((Math.random() * 100) + 0);
-                    var tasksProgress = Math.floor((Math.random() * 100) + 0);
-
-                    var tasksRecord = [];
-                    for (var i = 0; i < 30; i++) {
-                        var ts = new Date(2016, 1, i, 12, 30, 0, 0);
-                        var assigned = Math.floor((Math.random() * 10) + 0);
-                        var submitted = Math.floor((Math.random() * 10) + 0);
-                        var accepted = Math.floor((Math.random() * 10) + 0);
-                        var record = {ts: ts, assigned: assigned, submitted: submitted, accepted: accepted};
-                        tasksRecord.push(record);
-                    }
-                    //end compute analytics
-
-                    analytics[USER_ANALYTICS.GQM_SCORE] = gqmScore;
-                    analytics[USER_ANALYTICS.GRIDS_TOT] = gridsTot;
-                    analytics[USER_ANALYTICS.ARTIFACTS_TOT] = artifactsTot;
-                    analytics[USER_ANALYTICS.ACCEPTANCE_RATIO] = acceptanceRatio;
-                    analytics[USER_ANALYTICS.ARTIFACTS_MGOAL] = artifactsMGoal;
-                    analytics[USER_ANALYTICS.ARTIFACTS_RATE] = artifactsRate;
-                    analytics[USER_ANALYTICS.WORKTIME_RATIO] = worktimeRatio;
-                    analytics[USER_ANALYTICS.TASKS_PROGRESS] = tasksProgress;
-                    analytics[USER_ANALYTICS.TASKS_RECORD] = tasksRecord;
-
-                    resolve({analytics: analytics, username: username});
-                } else {
-                    reject({errmsg: 'User ' + username + ' not found'});
-                }
-            }, 500);
-        });
+        var request = {
+            method: 'GET',
+            url: REST_SERVICE.URL + '/analytics/users',
+            params: {
+                'username': username
+            }
+        };
+        return $http(request).then(
+            function(success) {
+                var analyticsDTO = angular.fromJson(success.data.analyticsDTO);
+                var userAnalytics = analyticsDTO[0];
+                return $q.resolve({analytics: userAnalytics});
+            },
+            function(error) {
+                var errmsg = angular.copy(error.data.message);
+                return $q.reject({errmsg: errmsg});
+            }
+        );
     }
 
 }
